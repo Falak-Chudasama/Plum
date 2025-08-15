35bdfb3842294379e62f4676bf6395fc0dae8ade
Falak-Chudasama <falakchudasama7766@gmail.com>
2025-08-15 12:51:05 +0530

    Fresh start

--- client/plum-client/src/apis/apis.ts
+++ client/plum-client/src/apis/apis.ts
@@ -1,8 +1,5 @@
 export default async function send() {
     await fetch('http://api.plum.com/user/send/email', {
-        method: "POST",
-        headers: {
-            "Content-Type": "",
-        }
+        method: "POST"
     });
 };

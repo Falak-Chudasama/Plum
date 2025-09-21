5523d7889be009cbe50934a0be16db9e23e02cbd
Falak-Chudasama <falakchudasama7766@gmail.com>
2025-09-21 20:48:21 +0530

    .

--- server/src/jobs/runJobs.ts
+++ server/src/jobs/runJobs.ts
@@ -27,8 +27,6 @@ const runJobs = async () => {
 
     await OAuthObjectCheck(email);
     logger.info(`Waiting ${minutesDelay} minutes...`);
-    await startCategoriesFeedingJob(); // del it
-    await startGmailFetcherJob(); // del it
     setInterval(async () => {
         try {
             if (globals.categoriesFeedingJobRunning === false) {
